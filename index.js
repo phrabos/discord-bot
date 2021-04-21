2f73584f7ae3fa1d727acdbc06429fd9c344559b
patrick hrabos <phrabos@gmail.com>
2021-04-21 18:33:42 -0400

    testing logic for bot  kill switch

--- index.js
+++ index.js
@@ -11,28 +11,63 @@ client.once('ready', ()=>{
 
 client.login(process.env.TOKEN);
 
-client.on('message', message =>{
-  // if(message.content.includes('working')) message.channel.send('it works...')
-  if(message.content.includes('!weather')){
-    let zipcode = message.content.split(" ")[1];
-    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${process.env.WEATHER_API_KEY}`)
-      .then(res =>{
-        return res.json()
-      })
-      .then(parsedWeather => {
-        message.channel.send(
-          `The Current Weather
-          Location: ${parsedWeather.name}
-          Forecast: ${parsedWeather.weather[0].main}
-          Current Temp: ${parsedWeather.main.temp} F
-          `
-        )
-      })
-  }
+client.on('message', async message =>{
+
+  
   if(message.content.includes('!setup')){
-    message.lineReplyNoMention('ready to setup...')
+
+    const sender= message.author.username
+
+    message.reply('timeout started')
     
+    let timeoutObj = client.setTimeout( () => { 
+      console.log('timeout complete');
+      message.reply('no longer shamed, rejoins society');
+     }, 20000);
+     
+
+  client.on('message', message=>{
+    
+    if(message.author.bot)return
+    if(message.content.includes('stop') && message.author.username === sender){
+      client.clearTimeout(timeoutObj);
+      message.reply('timer ended')
+      console.log('timer ended')
+      }
+    }) 
   }
-});
+
+})
+
+// .then(async ()=>{
+  //   message.channel.awaitMessages(()=>{}, {time: 15000})
+  //     if(message.content.includes('30')){
+    //       message.lineReply('timer started')
+    
+    // const filter = m => m.content.includes('!time')
+    // const collector = channel.createMessageCollector(filter, {time: 8000})
+    // collector.on('collect ', message=> {
+      //   console.log('message is:', message.content)
+//   message.lineReply('timer started')
+// })
 
 
+
+// message.lineReply('ready to setup! Tell me how many minutes you want to be productive...')
+
+// if(message.content.includes('!weather')){
+//   let zipcode = message.content.split(" ")[1];
+//   fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${process.env.WEATHER_API_KEY}`)
+//     .then(res =>{
+//       return res.json()
+//     })
+//     .then(parsedWeather => {
+//       message.channel.send(
+//         `The Current Weather
+//         Location: ${parsedWeather.name}
+//         Forecast: ${parsedWeather.weather[0].main}
+//         Current Temp: ${parsedWeather.main.temp} F
+//         `
+//       )
+//     })
+// }
