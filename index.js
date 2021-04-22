70f14579e836f6e9f762c57caf4f15b022a114d2
patrick hrabos <phrabos@gmail.com>
2021-04-22 11:23:14 -0400

    broken setup wizard

--- index.js
+++ index.js
@@ -36,37 +36,62 @@ client.on('message', async message =>{
       }
     }) 
   }
-  if(message.content.includes('!config')){
+
+  // if(message.content.includes('!config')){
+  //   const configSteps = {
+  //     validTimer: false,
+  //   }
+  //   const sender = message.author.username
+  //   if(!message.author.bot)
+  //   message.reply('pick from three levels of persistence: 1)mild 2)medium 3)sadistic')
+  //   client.on('message', message =>{
+  //     if(message.author.bot)return
+  //     if(!Number(message.content) && !message.author.bot){
+  //       return message.reply('must enter a number between 1 - 3')
+  //     }
+  //     if(message.content === '1' || message.content === '2' || message.content === '3'){
+  //       message.reply('ready to set up! Tell me how many milliseconds you want to be productive...')
+  //       client.on('message', message =>{
+  //         if(message.author.bot)return
+  //         if(message.author.username === sender){
+  //         if(!Number(message.content)){
+  //           return message.reply('must enter a number')
+  //         }
+  //         if(message.content){
+  //           message.reply(`timer set for ${message.content} milliseconds`)
+  //           message.reply(`you are now in shame-mode`)
+  //           setTimeout(() => {
+  //             message.reply('now out of shame-mode, carry on')
+  //           }, message.content);
+  //         }
+  //       }
+          
+  //       })
+  //     }
+  //   })
+  // }
+  if(message.content.includes('!switch')){
     const configSteps = {
-      validTimer: false,
+      configStep: 0,
     }
     const sender = message.author.username
     if(!message.author.bot)
     message.reply('pick from three levels of persistence: 1)mild 2)medium 3)sadistic')
     client.on('message', message =>{
       if(message.author.bot)return
-      if(!Number(message.content) && !message.author.bot){
-        return message.reply('must enter a number between 1 - 3')
+      const expression = message.content;
+      switch(expression){
+        case '1': message.reply('small amount of restrictions applied');
+          break;
+        case '2': message.reply('medium amount of restrictions applied');
+          break;
+        case '3': message.reply('sadistic amount of restrictions applied');
+          break;
+        default:  ('must enter a number between 1 - 3')
       }
-      if(message.content === '1' || message.content === '2' || message.content === '3'){
-        message.reply('ready to set up! Tell me how many milliseconds you want to be productive...')
-        client.on('message', message =>{
-          if(message.author.bot)return
-          if(message.author.username === sender){
-          if(!Number(message.content)){
-            return message.reply('must enter a number')
-          }
-          if(message.content){
-            message.reply(`timer set for ${message.content} milliseconds`)
-            message.reply(`you are now in shame-mode`)
-            setTimeout(() => {
-              message.reply('now out of shame-mode, carry on')
-            }, message.content);
-          }
-        }
-          
-        })
-      }
+      message.reply('Tell me how many milliseconds you want to be productive...')
+
+      
     })
   }
 })
